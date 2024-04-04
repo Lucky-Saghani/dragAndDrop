48fb28dae225b9273adb6fe3b361fabf5416235c
Lucky <luckey.saghani@hotmail.com>
2024-04-04 16:05:15 +0200

    Using autobinders to bind

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,21 @@
+// autobnd decorator
+function autobind(
+    _: any,
+    _2: string, 
+    descriptor: PropertyDescriptor
+    ) {
+        const originalMethod = descriptor.value;
+        const adjDescriptor: PropertyDescriptor = {
+            configurable: true,
+            get() {
+                const boundFn = originalMethod.bind(this);
+                return boundFn;
+            }
+        };
+        return adjDescriptor;
+}
+
+// ProjectInput Class
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -18,17 +36,18 @@ class ProjectInput {
         this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
         this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
        
-       this.configure();
+        this.configure();
         this.attach();
     }
 
+    @autobind
     private submitHandler(event: Event){
         event.preventDefault();
         console.log(this.titleInputElement.value);
     }
 
     private configure() {
-        this.element.addEventListener('submit', this.submitHandler.bind(this));
+        this.element.addEventListener('submit', this.submitHandler);
     }
 
     private attach() {
